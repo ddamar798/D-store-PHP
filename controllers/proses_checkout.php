6a6c809a24cc7e39cfeb7336a9da7e07bf5b899a
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-21 19:19:41 +0700

    feat: update proses checkout

--- controllers/proses_checkout.php
+++ controllers/proses_checkout.php
@@ -1,5 +1,6 @@
 <?php
-include '../includes/koneksi.php';
+include '../config/koneksi.php';
+session_start();
 
 $nama     = $_POST['nama'];
 $email    = $_POST['email'];
@@ -7,13 +8,23 @@ $alamat   = $_POST['alamat'];
 $no_hp    = $_POST['no_hp'];
 $produk_id = $_POST['produk_id'];
 $qty      = $_POST['qty'];
-$total    = $_POST['total_harga'] * $qty;
+$harga    = $_POST['harga'];
+$total    = $harga * $qty;
 
 $query = "INSERT INTO checkout (nama, email, alamat, no_hp, produk_id, qty, total_harga) 
           VALUES ('$nama', '$email', '$alamat', '$no_hp', '$produk_id', '$qty', '$total')";
 
-if(mysqli_query($koneksi, $query)) {
-    echo "<script>alert('Checkout berhasil!'); window.location.href='../public/index.php';</script>";
+if(mysqli_query($db, $query)) {
+    // kosongkan keranjang
+    unset($_SESSION['keranjang']);
+
+    echo "<script>
+        alert('Checkout berhasil! Terima kasih telah berbelanja di Dstore.');
+        window.location.href = '../public/index.php';
+    </script>";
 } else {
-    echo "<script>alert('Checkout gagal!'); window.history.back();</script>";
+    echo "<script>
+        alert('Checkout gagal. Silakan coba lagi.');
+        window.history.back();
+    </script>";
 }
