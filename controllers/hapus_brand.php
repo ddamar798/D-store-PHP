a2c69721f10225906a49ff8002d19c4954c968c3
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-21 20:12:50 +0700

    fix bug

--- controllers/hapus_brand.php
+++ controllers/hapus_brand.php
@@ -4,7 +4,7 @@ include_once '../includes/auth_admin.php';
 include_once '../config/db.php';
 
 $id = (int) $_GET['id'];
-$query = mysqli_query($db, "DELETE FROM brand WHERE id = $id");
+$query = mysqli_query($db, "DELETE FROM brands WHERE id = $id");
 
 header("Location: ../admin/brand.php");
 exit;
